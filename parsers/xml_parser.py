62c3381bc0c7377cf990563e6204c56ce34efae0
bakrfrag <bakr.frag@gmail.com>
2021-03-28 20:53:28 +0200

    add xml parser to parse xml files

--- parsers/xml_parser.py
+++ parsers/xml_parser.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3.8;
+import untangle;
+class XmlParser(object):
+    def __init__(self,input_file,export_format="xml"):
+        self.input_file=input_file;
+        self.export_format=export_format;
+        self.file_data="";
+    
