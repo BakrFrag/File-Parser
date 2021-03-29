e3873f5d1074ff15582ce327ffcdf02593a58eca
bakrfrag <bakr.frag@gmail.com>
2021-03-29 06:08:43 +0200

    add class parser handler to handle arguments and apply parsers to arguments

--- parsers.py
+++ parsers.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3.8
+from parser import csv_parser,xlsx_parser,xml_parser;
+import sys;
+class ParserHandler(object):
+    """
+    handle parsed arguments
+    """
+    FORMATS=["xml","csv","xlsx"];
+    def __init__(self,arguments):
+        self.arguments=sys.argv;
+        self.length=len(self.arguments);
+        self.handle_arguments();
+        
