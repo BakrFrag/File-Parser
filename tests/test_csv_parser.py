fbce7d40de80b0f4010a85fe6df4ac861b64c9fc
bakrfrag <bakr.frag@gmail.com>
2021-03-29 11:06:20 +0200

    add function to test read csv file with headers miss match

--- tests/test_csv_parser.py
+++ tests/test_csv_parser.py
@@ -20,3 +20,14 @@ class TestCsvParser(unittest.TestCase):
         function_result=csv_parser.read_csv(self.customers_file,kind="customer");
         self.assertEqual(function_result['error'],"file {} not exists".format(self.csv_vehicles_not_exist));
     def test_with_headers_miss_match(self):
+        """
+        test CsvParser with customer and vehicle file headers miss match
+        """
+        csv_parser=CsvParser(self.valid_customers,self.csv_vehicle_headers_miss_match);
+        function_result=csv_parser.read_csv(self.vehicles_file,kind="vehicle");
+        self.assertEqual(function_result['error'],"file of type vehicle headers don't match");
+
+
+        csv_parser=CsvParser(self.csv_customer_headers_miss_match,self.valid_vehicles);
+        function_result=csv_parser.read_csv(self.customers_file,kind="vehicle");
+        self.assertEqual(function_result['error'],"file of type customer headers don't match");
