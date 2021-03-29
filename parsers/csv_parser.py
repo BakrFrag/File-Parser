47193cccaf9e0e6ab86bfa2e06af1401ff4d626e
bakrfrag <bakr.frag@gmail.com>
2021-03-29 05:40:21 +0200

    add function to read from csv file if exists

--- parsers/csv_parser.py
+++ parsers/csv_parser.py
@@ -11,4 +11,27 @@ class CsvParser(object):
               self.vehicles_file=vehicles;
               self.customers_data=[];
               self.vehicles_data=[];
-    
+     def read_csv(self,input_file,kind="customer"):
+        """
+        read data from csv file if exists
+        """
+        try:
+            if FileParserHelper.check_file_exist(input_file):
+                    csv_data=[];
+                    with open(input_file,"r") as csv_content:
+                        rows=csv.DictReader(csv_content);
+                        headers=rows.fieldnames;
+                        print(headers)
+                        if FileParserHelper.check_file_headers(headers,kind):
+                                    print("headers matched")
+                                    for row in rows:
+                                        if row not in csv_data:
+                                             
+                                               csv_data.append(row);
+                                    return csv_data;
+                        else:
+                            return {"error":"file of type {} headers don't match".format(kind)};
+            else:
+                        return {"error":"file {} not exists".format(input_file)}
+        except Exception as E:
+                return {"error":"the file {} not valid csv file".format(input_file)};
