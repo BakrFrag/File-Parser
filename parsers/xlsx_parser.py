b1de942bc6d36422609ab0bd4f999729d5077ab8
bakrfrag <bakr.frag@gmail.com>
2021-03-29 05:46:44 +0200

    add function to read xlsx file data

--- parsers/xlsx_parser.py
+++ parsers/xlsx_parser.py
@@ -1,1 +1,37 @@
 #!/usr/bin/python3.8
+import xlrd;
+from helpers import ParserHelper,FileParserHelper
+class XlsxParser(object):
+    """
+    init parameters used to parse xlsx file
+    """
+    def __init__(self,customers,vehicles):
+              self.format="xlsx";
+              self.customers_file=customers;
+              self.vehicles_file=vehicles;
+              self.customers_data=[];
+              self.vehicles_data=[];
+    def read_xlsx(self,input_file,kind="customer"):
+        """
+        parse xlsx file and read data if exists
+        """
+        try:
+            if FileParserHelper.check_file_exist(input_file):
+                        xlsx_data=[];
+                        xlsx_workbook=xlrd.open_workbook(input_file);
+                        xlsx_sheet=xlsx_workbook.sheet_by_index(0);
+                        headers=xlsx_sheet.row_values(0);
+                        if FileParserHelper.check_file_headers(headers,kind):
+                                    xlsx_rows=xlsx_sheet.nrows;
+                                    for i in range(1,xlsx_rows):
+                                        row=xlsx_sheet.row_values(i);
+                                        if row not in xlsx_data:
+                                            xlsx_data.append(row)
+                                    return xlsx_data;    
+                        else:
+                                return {"error":"file of type {} headers don't match".format(kind)};
+            else:
+                        return {"error":"file {} not exists".format(input_file)}
+                            
+        except Exception as E:
+                        return {"error":"the file {} not valid xlsx file".format(input_file)};
