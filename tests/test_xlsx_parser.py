02d4c04f4aa60877721a3459672b62fddc8af87f
bakrfrag <bakr.frag@gmail.com>
2021-03-29 11:17:28 +0200

    add function to test xlsx parser with headers file miss match

--- tests/test_xlsx_parser.py
+++ tests/test_xlsx_parser.py
@@ -11,3 +11,15 @@ class TestCsvParser(unittest.TestCase):
         self.xlsx_vehicle_headers_miss_match="vehicles_headers_error.xlsx";
         self.xlsx_customers_not_exist="datasets/customers_not_exists.xlsx";
         self.xlsx_vehicles_not_exist="datasets/vehicles_not_exists.xlsx";
+
+    def test_with_file_not_exist(self):
+        """
+        test XlsxParser with file not exist
+        """
+        xlsx_parser=xlsxParser(self.valid_customers,self.xlsx_vehicles_not_exist);
+        function_result=xlsx_parser.read_xlsx(self.vehicles_file,kind="vehicle");
+        self.assertEqual(function_result['error'],"file {} not exists".format(self.xlsx_vehicles_not_exist));
+
+        xlsx_parser=XlsxParser(self.xlsx_customers_not_exist,self.valid_vehicles);
+        function_result=xlsx_parser.read_xlsx(self.customers_file,kind="customer");
+        self.assertEqual(function_result['error'],"file {} not exists".format(self.xlsx_vehicles_not_exist));
