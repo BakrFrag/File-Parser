e9e0bf1f2c3d3948df56d9202f5c6f81412ee66b
bakrfrag <bakr.frag@gmail.com>
2021-03-29 05:39:29 +0200

    add csv praser class to parse csv file

--- parsers/csv_parser.py
+++ parsers/csv_parser.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3.8
+import csv;
+from helpers import ParserHelper,FileParserHelper;
+class CsvParser(object):
+    """
+    init parameters used to parse csv data  
+    """
+    def __init__(self,customers,vehicles):
+              self.format="csv";
+              self.customers_file=customers;
+              self.vehicles_file=vehicles;
+              self.customers_data=[];
+              self.vehicles_data=[];
+    
